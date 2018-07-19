1ca46371bb9a48ef0af8f0aa5ee37210884cf848
re0marb1e <zhxt1016@gmail.com>
2018-07-19 23:12:39 +0800

    add more brew and npm package

--- brew.sh
+++ brew.sh
@@ -8,12 +8,40 @@ brew install git
 brew install wget
 brew install tree
 brew install aria2
+brew install tmux
+brew install imagemagick
 brew install proxychains-ng
+brew install e2fsprogs
 
 # env
+
+brew install php
 brew install node
 brew install io
 
+# project management tools
+
+brew install composer
+brew install maven # jdk required and conflicts with mvnvm
+brew install gradle # jdk required
+
+# db
+
+brew install redis
+brew install mysql
+brew install mongodb
+
+# server
+
+brew install tomcat # jdk required
+brew install nginx
+brew install httpd
+
+# big data
+
+brew install zookeeper # jdk required
+
 # env management tools
+
 brew install jenv
 brew install pyenv
--- npm.sh
+++ npm.sh
@@ -1,6 +1,7 @@
 
 npm install nodemon -g
 npm install cnpm -g
+npm install yarn -g
 npm install hexo-cli -g
 npm install commitizen -g
 npm install pm2 -g
@@ -9,3 +10,24 @@ npm install hiper -g
 npm install mocha -g
 npm install standard-version -g
 npm install create-react-app -g
+
+# npm install -g aglio
+# npm install -g bower
+# npm install -g drakov
+# npm install -g docsify-cli
+# npm install -g express-generator
+# npm install -g fleet
+# npm install -g flatiron
+# [sudo] npm install forever -g
+# npm install -g generator-alfred
+# npm install -g yo generator-kraken bower
+# npm install -g grunt-cli
+# npm install -g gulp-cli
+# npm install -g jshint
+# npm install -g less
+# npm install -g nrm
+# npm install -g pug-cli
+# npm install -g sails
+# npm install -g strongloop
+# npm install -g vue-cli
+# npm install -g yo
