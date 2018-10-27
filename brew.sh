ed2b26061e283590fcfb250b74ca8e7a9f834554
re0marb1e <zhxt1016@gmail.com>
2018-10-27 15:22:43 +0800

    add mannual.sh

--- brew.sh
+++ brew.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-############################## install homebrew ##############################
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-############################## install git ##############################
-brew install git
-
 ############################## install oh-my-zsh ##############################
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
---
+++ mannual.sh
@@ -0,0 +1,9 @@
+
+############################## install homebrew ##############################
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+############################## install git ##############################
+brew install git
+
+############################## git clone this repository ##############################
+git clone https://github.com/re0marb1e/scripts-after-macOS-install ~/Documents/WorkHome/GitHub/scripts-after-macOS-install
